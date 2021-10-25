f5eb1b983d86428372acc95524b50981a25d0022
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-10-25 08:35:47 -0300

    Update 003.cpp

--- exercicios/003.cpp
+++ exercicios/003.cpp
@@ -1,19 +1,15 @@
 /*
 Faça um Programa que peça dois números e imprima a soma.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float num1, num2, soma;
-    printf("Digite um numero: ");
-    scanf("%f", &num1);
-    printf("Digite outro numero: ");
-    getchar();
-    scanf("%f", &num2);
-    soma = num1 + num2;
-    printf("A soma eh: %f", soma);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    int n1=0, n2=0;
+    std::cout << "Digite dois numeros\n";
+    std::cin >> n1 >> n2;
+    std::cout << "A soma dos numeros e: " << n1+n2;
+
     return 0;
 }
