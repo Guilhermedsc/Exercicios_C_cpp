4f9d1cf6262069e17a2f414a1ec095f6018346a4
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 20:12:15 -0300

    Update 021.cpp

--- exercicios/021.cpp
+++ exercicios/021.cpp
@@ -2,26 +2,20 @@
 Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
     char sexo;
-    printf("Digite F para Feminino ou M para Masculino: ");
-    scanf("%c", &sexo);
-    if (sexo == 'F' || sexo == 'f')
-    {
-        printf("Feminino");
+
+    cout << "Digite a letra referente ao sexo: ";
+    cin >> sexo;
+
+    if(sexo == 'M' || sexo == 'm'){
+        cout << "Masculino" << endl;
+    }else if(sexo == 'F' || sexo == 'f'){
+        cout << "Feminino" << endl;
+    }else{
+        cout << "Sexo invalido" << endl;
     }
-    else if (sexo == 'M' || sexo == 'm')
-    {
-        printf("Masculino");
-    }
-    else
-    {
-        printf("Sexo inválido");
-    }
-    return 0;
 }
