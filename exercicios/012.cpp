4837d05b74f467021058ca2d0d236bb2259b6855
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 10:39:25 -0300

    Update 012.cpp

--- exercicios/012.cpp
+++ exercicios/012.cpp
@@ -3,16 +3,17 @@ Tendo como dados de entrada a altura de uma pessoa,
 construa um algoritmo que calcule seu peso ideal,
 usando a seguinte fórmula: (72.7*altura) - 58
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float altura, pesoIdeal;
-    printf("Digite sua altura: ");
-    scanf("%f", &altura);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float altura=0, pesoIdeal;
+
+    cout << "Digite sua altura: ";
+    cin >> altura;
+
     pesoIdeal = (72.7 * altura) - 58;
-    printf("Seu peso ideal eh: %.2fkg", pesoIdeal);
+
+    cout << "Seu peso ideal: " << pesoIdeal;
     return 0;
 }
