a68cf4cb9d4f04bb9f2b6fcd1257dbce1bee1dd7
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 10:48:35 -0300

    Update 013.cpp

--- exercicios/013.cpp
+++ exercicios/013.cpp
@@ -5,19 +5,18 @@ utilizando as seguintes fórmulas:
     Para homens: (72.7*h) - 58
     Para mulheres: (62.1*h) - 44.7
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float altura, pesoIdealH, pesoIdealM;
-    printf("Digite sua altura em m: ");
-    scanf("%f", &altura);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float altura=0, pesoIdealH=0, pesoIdealM=0;
+
+    cout << "Digite sua altura: ";
+    cin >> altura;
+
     pesoIdealH = (72.7 * altura) - 58;
     pesoIdealM = (62.1 * altura) - 44.7;
-    printf(
-        "O seu peso ideal eh:\n%.2fkg se voce for homem\n%.2fkg se voce for mulher",
-        pesoIdealH, pesoIdealM);
+
+    cout << "Seu peso ideal para o sexo masculino: " << pesoIdealH << "\nSeu peso ideal para o sexo feminino: " << pesoIdealM << endl;
     return 0;
 }
