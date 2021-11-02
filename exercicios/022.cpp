6cb4769e032c971c19afbc9990bd1aaa6dca2922
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 20:19:06 -0300

    Update 022.cpp

--- exercicios/022.cpp
+++ exercicios/022.cpp
@@ -1,75 +1,21 @@
 /*
 Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
 
-void forma1()
-{
+int main(){
     char letra;
-    printf("Digite uma letra: ");
-    scanf("%c", &letra);
-    if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' || letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
-    {
-        printf("Vogal");
-    }
-    else
-    {
-        printf("Consoante");
-    }
-}
 
-void forma2()
-{
-    char letra;
-    printf("Digite uma letra: ");
-    scanf("%c", &letra);
+    cout << "Digite uma letra: ";
+    cin >> letra;
 
-    // toupper está na biblioteca ctype
-    //Agora a letra é maiúscula, e assim reduzimos as comparações
+    //transforma letra em maiuscula
     letra = toupper(letra);
 
-    if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
-    {
-        printf("Vogal");
+    if(letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
+           cout << "Vogal" << endl;
+    }else{
+        cout << "Consoante" << endl;
     }
-    else
-    {
-        printf("Consoante");
-    }
-}
-
-void forma3()
-{
-    char letra;
-    printf("Digite uma letra: ");
-    scanf("%c", &letra);
-    switch (letra)
-    {
-    case 'A':
-    case 'E':
-    case 'I':
-    case 'O':
-    case 'U':
-    case 'a':
-    case 'e':
-    case 'i':
-    case 'o':
-    case 'u':
-        printf("Vogal");
-        break;
-    default:
-        printf("Consoante");
-        break;
-    }
-}
-
-int main()
-{
-    forma1();
-    // forma2();
-    // forma3();
-    return 0;
 }
