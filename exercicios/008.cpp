f59b22b7f68d99e191e289a5019072b3b0b6524f
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 10:01:29 -0300

    Update 008.cpp

--- exercicios/008.cpp
+++ exercicios/008.cpp
@@ -2,19 +2,19 @@
 Faça um Programa que pergunte quanto você ganha por hora e o número de horas
 trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float salarioHr, salarioMe, horasTrabalhadas;
-    printf("Digite quanto voce ganha por hora: ");
-    scanf("%f", &salarioHr);
-    printf("Digite quantas horas voce trabalhou esse mes: ");
-    getchar();
-    scanf("%f", &horasTrabalhadas);
-    salarioMe = salarioHr * horasTrabalhadas;
-    printf("Seu salario total do mes eh de R$%.2f", salarioMe);
+#include <bits/stdc++.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float ganho_hora=0, horas_mes=0;
+
+    std::cout << "Digite o valor do seu salario por hora: ";
+    std::cin >> ganho_hora;
+
+    std::cout << "Digite a quantidade de horas que trabalha por mês: ";
+    std::cin >> horas_mes;
+
+    std::cout << "Seu salario por mês é: " << ganho_hora * horas_mes << std::endl;
+
     return 0;
 }
