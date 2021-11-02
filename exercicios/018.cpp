4064ecae4a4ad70e67d01c4c33f1a9db62a174cc
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-02 14:48:26 -0300

    Update 018.cpp

--- exercicios/018.cpp
+++ exercicios/018.cpp
@@ -3,25 +3,17 @@ Faça um programa que peça o tamanho de um arquivo para download (em MB) e a
 velocidade de um link de Internet (em Mbps), calcule e informe o tempo
 aproximado de download do arquivo usando este link (em minutos).
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float tamanhoDoArquivo,
-        velocidadeDaInternet,
-        tamanhoDoArquivoEmMegaBits,
-        tempoDeDownload;
-    printf("Digite o tamanho do arquivo em MB (Mega Bytes): ");
-    scanf("%f", &tamanhoDoArquivo);
-    printf("Digite a velocidade da internet em Mbps (Mega bits por segundo): ");
-    scanf("%f", &velocidadeDaInternet);
-    // Eu criei a variável abaixo apenas para deixar claro que 1MB são 8Mb.
-    // Ela não é totalmente necessária
-    tamanhoDoArquivoEmMegaBits = tamanhoDoArquivo * 8;
-    tempoDeDownload = tamanhoDoArquivoEmMegaBits / velocidadeDaInternet;
-    printf("O tempo aproximado para o download do arquivo é de %.0f segundos",
-           tempoDeDownload);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float tamanho_arquivo=0, velocidade_net=0, temp_download=0;
+
+    cout << "Digite o tamanho do arquivo em MB e a velocidade da internet em Mbps, respectivamente:" << endl;
+    cin >> tamanho_arquivo >> velocidade_net;
+
+    temp_download = (tamanho_arquivo * 8 ) / velocidade_net;
+
+    cout << "O tempo de download do arquivo sera de " << temp_download << " segundos." << endl;
     return 0;
 }
