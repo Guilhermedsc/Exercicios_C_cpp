970c04a75b5946b8813c2e95dd7f443958e0ee90
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-03 08:19:14 -0300

    Update 027.cpp

--- exercicios/027.cpp
+++ exercicios/027.cpp
@@ -1,30 +1,31 @@
 /*
 Faça um Programa que leia três números e mostre-os em ordem decrescente.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    float numero1, numero2, numero3;
-    printf("Digite um numero: ");
-    scanf("%f", &numero1);
-    printf("Digite outro numero: ");
-    scanf("%f", &numero2);
-    printf("Digite mais um numero: ");
-    scanf("%f", &numero3);
-    if (numero1 > numero2 && numero2 > numero3)
-        printf("%f %f %f", numero1, numero2, numero3);
-    else if (numero1 > numero3 && numero3 > numero2)
-        printf("%f %f %f", numero1, numero3, numero2);
-    else if (numero2 > numero1 && numero1 > numero3)
-        printf("%f %f %f", numero2, numero1, numero3);
-    else if (numero2 > numero3 && numero3 > numero1)
-        printf("%f %f %f", numero2, numero3, numero1);
-    else if (numero3 > numero1 && numero1 > numero2)
-        printf("%f %f %f", numero3, numero1, numero2);
-    else
-        printf("%f %f %f", numero3, numero2, numero1);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float v[3];
+    float aux=0;
+
+    cout << "Digite 3 numeros para ordená-los:" << endl;
+    for(int i=0; i<3; i++){
+        cin >> v[i];
+    }
+
+    for(int i=0; i<3; i++){
+        for(int j=i; j<3; j++){
+            if(v[i] < v[j+1]){
+                aux = v[j+1];
+                v[j+1] = v[i];
+                v[i] = aux;
+            }
+        }    
+    }
+    
+    for(int i=0; i<3; i++){
+        cout << v[i] << endl;
+    }
+
     return 0;
 }
