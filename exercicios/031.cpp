c388acae07fb66566d0f6b8fd33f787623eb92bc
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-04 11:00:16 -0300

    Update 031.cpp

--- exercicios/031.cpp
+++ exercicios/031.cpp
@@ -3,48 +3,26 @@ Faça um Programa que leia um número e exiba o dia correspondente da semana.
 (1-Domingo, 2- Segunda, etc.),
 se digitar outro valor deve aparecer valor inválido.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    //Para esse código funcionar precisamos incluir a biblioteca de string
-    //O comando (fora da main) é #include <cstring>
-    //Ou, em c, #include <string.h>
-    int diaInt;
-    //A linha abaixo cria uma string, que nada mais é do que um
-    //vetor de caracteres
-    //Uma variável char guarda 1 letra. Uma string guarda várias letras
-    //O número de letras limite é o que está em colchetes, neste caso 40
-    char diaStr[40] = "";
-    printf("Digite o número do dia da semana: ");
-    scanf("%i", &diaInt);
+#include <bits/stdc++.h>
+using namespace std;
 
-    if (diaInt == 1)
-        //A strcpy copia uma string (neste caso domingo)
-        //para uma variável do tipo string
-        //Não dá para simplesmente fazer algo como diaStr = "Domingo";
-        strcpy(diaStr, "Domingo");
-    else if (diaInt == 2)
-        strcpy(diaStr, "Segunda");
-    else if (diaInt == 3)
-        strcpy(diaStr, "Terça");
-    else if (diaInt == 4)
-        strcpy(diaStr, "Quarta");
-    else if (diaInt == 5)
-        strcpy(diaStr, "Quinta");
-    else if (diaInt == 6)
-        strcpy(diaStr, "Sexta");
-    else if (diaInt == 7)
-        strcpy(diaStr, "Sábado");
+int main(){
+    int dia=0;
 
-    //strcmp compara duas strings. Se forem iguais é retornado 0
-    //Caso a string não esteja vazia, entramos no if
-    if (strcmp(diaStr, ""))
-        printf("O dia correspondente é %s", diaStr);
-    else
-        printf("Valor inválido");
+    cout<<"Digite o dia da semana: \n";
+    while(true){
+        cout << "$ ";
+        cin >> dia;
+
+        if(dia == 1) cout<<"Segunda-feira"<<endl;
+        else if(dia == 2) cout<<"Terca-feiraa"<<endl;
+        else if(dia == 3) cout<<"Quarta-feira"<<endl;
+        else if(dia == 4) cout<<"Quinta-feira"<<endl;
+        else if(dia == 5) cout<<"Sexta-feira"<<endl;
+        else if(dia == 6) cout<<"Sabado-feira"<<endl;
+        else if(dia == 7) cout<<"Domingo-feira"<<endl;
+        else cout << "Dia invalido!" << endl; 
+    }
 
     return 0;
 }
