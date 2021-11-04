7cb2efc7fd3651f96074ebbad850c101ddbe6b77
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-04 11:11:26 -0300

    Update 032.cpp

--- exercicios/032.cpp
+++ exercicios/032.cpp
@@ -14,36 +14,25 @@ O algoritmo deve mostrar na tela as notas, a média,
 o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C
 ou “REPROVADO” se o conceito for D ou E.
 */
-#include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
-int main()
-{
-    char aproveitamento;
-    float nota1, nota2, media;
+#include <bits/stdc++.h>
+using namespace std;
 
-    printf("Digite a primeira nota do semestre: ");
-    scanf("%f", &nota1);
-    printf("Digite a segunda nota do semestre: ");
-    scanf("%f", &nota2);
+int main(){
+    char aproveitamento=0;
+    float n1=0, n2=0, media=0;
 
-    media = (nota1 + nota2) / 2;
+    cout << "Digite as duas notas respectivamente: " << endl;
+    cin >> n1 >> n2;
 
-    if (media >= 9)
-        aproveitamento = 'A';
-    else if (media >= 7.5)
-        aproveitamento = 'B';
-    else if (media >= 6)
-        aproveitamento = 'C';
-    else if (media >= 4)
-        aproveitamento = 'D';
-    else
-        aproveitamento = 'E';
+    media = (n1 + n2) / 2;
 
-    if (aproveitamento == 'D' || aproveitamento == 'E')
-        printf("REPROVADO\nAproveitamento: %c", aproveitamento);
-    else
-        printf("APROVADO\nAproveitamento: %c", aproveitamento);
+    if(media >= 9) aproveitamento = 'A';
+    else if(media >= 7.5) aproveitamento = 'B';
+    else if(media >= 6) aproveitamento = 'C';
+    else if(media >= 4) aproveitamento = 'D';
+    else aproveitamento = 'E';
+
+    if(aproveitamento != 'D' && aproveitamento != 'E') cout << "Aprovado com " << aproveitamento << endl;
+    else cout << "Reprovado com " << aproveitamento << endl;
     return 0;
 }
