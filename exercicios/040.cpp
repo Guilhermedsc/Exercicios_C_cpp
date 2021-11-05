75d9239c64135285382e030f31ae50687e42cccf
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-05 11:08:20 -0300

    Update 040.cpp

--- exercicios/040.cpp
+++ exercicios/040.cpp
@@ -2,18 +2,20 @@
 Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
 Dica: utilize uma função de arredondamento.
 */
-#include <cmath>
-#include <cstring>
-#include <ctype.h>
-#include <stdio.h>
-int main()
-{
-    float numero;
-    printf("Digite um número: ");
-    scanf("%f", &numero);
-    if (floor(numero) == numero)
-        printf("Inteiro");
-    else
-        printf("Decimal");
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float n=0;
+
+    cout << "Digite um numero:" << endl;
+    while(true){
+        cout << "$ ";
+        cin >> n;
+
+        if(int(n) == n) cout << "Numero inteiro" << endl;
+        else cout << "Numero decimal" << endl;
+        endl(cout);
+    }
     return 0;
 }
