650d582802ce6e8ec1623bfc8c1094fe136fca52
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-05 10:57:46 -0300

    Update 038.cpp

--- exercicios/038.cpp
+++ exercicios/038.cpp
@@ -18,38 +18,36 @@ Exemplo 2:
 Para sacar a quantia de 399 reais, o programa fornece três notas de 100,
 uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
 */
-#include <cmath>
-#include <cstring>
-#include <ctype.h>
-#include <stdio.h>
-int main()
-{
-    int valor, cem, cinquenta, dez, cinco, um;
-    printf("Digite o valor a ser sacado (entre 10 e 600): ");
-    scanf("%i", &valor);
-    if (valor < 10 or valor > 600)
-        printf("Valor inválido!");
-    else
-    {
-        cem = valor / 100;  // Pegamos a centena com uma divisão inteira
-        valor -= cem * 100; // Subtraímos as centenas retiradas do valor total
-        cinquenta = valor / 50; // Idem para as outras coisas
-        valor -= cinquenta * 50;
-        dez = valor / 10;
-        valor -= dez * 10;
-        cinco = valor / 5;
-        valor -= cinco * 5;
-        um = valor; // Depois de subtrair as de cinco só sobram as de um
-        if (cem > 0)
-            printf("\n%i nota(s) de cem", cem);
-        if (cinquenta > 0)
-            printf("\n%i nota(s) de cinquenta", cinquenta);
-        if (dez > 0)
-            printf("\n%i nota(s) de dez", dez);
-        if (cinco > 0)
-            printf("\n%i nota(s) de cinco", cinco);
-        if (um > 0)
-            printf("\n%i nota(s) de um", um);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int  valor=0, cem=0, cinquenta=0, dez=0, cinco=0, um=0;
+
+    cout << "Digite o valor a ser sacado (entre 10 e 600): " << endl;
+    while(true){
+        cout << "$ ";
+        cin >> valor;
+
+        if(valor < 10 || valor > 600) cout << "Valor invalido!" << endl;
+        else {
+            cem = valor / 100;
+            valor -= cem * 100;
+            cinquenta = valor / 50;
+            valor -= cinquenta * 50;
+            dez = valor / 10;
+            valor -= dez * 10;
+            cinco = valor / 5;
+            valor -= cinco * 5;
+            um = valor;
+
+            cout << "Notas de 100: " << cem << endl;
+            cout << "Notas de 50:  " << cinquenta << endl;
+            cout << "Notas de 10:  " << dez << endl;
+            cout << "Notas de 5:   " << cinco << endl;
+            cout << "Notas de 1:   " << um << endl;
+            endl(cout);
+        }
     }
     return 0;
 }
