40e1e8caae5817250c34fda5953e02586af5e6a9
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-06 11:23:05 -0300

    Update 042.cpp

--- exercicios/042.cpp
+++ exercicios/042.cpp
@@ -14,47 +14,37 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada
 como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
 Caso contrário, ele será classificado como "Inocente".
 */
-#include <cmath>
-#include <cstring>
-#include <ctype.h>
-#include <stdio.h>
-int main()
-{
-    char resposta;
-    int positivos = 0;
+#include <bits/stdc++.h>
+using namespace std;
 
-    printf("Telefonou para a vítima? (S ou N): ");
-    scanf("%c", &resposta);
-    if (resposta == 'S' || resposta == 's')
-        positivos++;
-    printf("Esteve no local do crime? (S ou N): ");
-    getchar();
-    scanf("%c", &resposta);
-    if (resposta == 'S' || resposta == 's')
-        positivos++;
-    printf("Mora perto da vítima? (S ou N): ");
-    getchar();
-    scanf("%c", &resposta);
-    if (resposta == 'S' || resposta == 's')
-        positivos++;
-    printf("Devia para a vítima? (S ou N): ");
-    getchar();
-    scanf("%c", &resposta);
-    if (resposta == 'S' || resposta == 's')
-        positivos++;
-    printf("Já trabalhou com a vítima? (S ou N): ");
-    getchar();
-    scanf("%c", &resposta);
-    if (resposta == 'S' || resposta == 's')
-        positivos++;
+int main(){
+    char resposta=0;
+    int n = 0;
 
-    if (positivos < 2)
-        printf("Inocente");
-    else if (positivos == 2)
-        printf("Suspeita");
-    else if (positivos < 5)
-        printf("Cúmplice");
-    else
-        printf("Assassino");
+    cout << "Telefonou para a vitima? (S ou N): ";
+    cin >> resposta;
+    if (toupper(resposta) == 'S') n++;
+
+    cout << "Esteve no local do crime? (S ou N): ";
+    cin >> resposta;
+    if (toupper(resposta) == 'S') n++;
+
+    cout << "Mora perto da vitima? (S ou N): ";
+    cin >> resposta;
+    if (toupper(resposta) == 'S') n++;
+
+    cout << "Devia para a vitima? (S ou N): ";
+    cin >> resposta;
+    if (toupper(resposta) == 'S') n++;
+
+    cout << "Ja trabalhou com a vitima? (S ou N): ";
+    cin >> resposta;
+    if (toupper(resposta) == 'S') n++;
+
+    if(n < 2) cout << "Inocente";
+    else if(n == 2) cout << "Suspeita";
+    else if(n < 5) cout << "Cúmplice";
+    else cout << "Assassino";
+     
     return 0;
 }
